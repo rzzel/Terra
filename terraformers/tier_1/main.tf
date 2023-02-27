7563920f379371306dbfe7c33f1248253337bd57
Ruzzel <123532716+rzzel@users.noreply.github.com>
2023-02-27 08:30:12 +0800

    Update main.tf

--- terraformers/tier_1/main.tf
+++ terraformers/tier_1/main.tf
@@ -3,7 +3,7 @@
 resource "aws_subnet" "private_subnets" {
   for_each   = var.private_subnets
   vpc_id     = data.aws_vpc.default.id
-  cidr_block = cidrsubnet(data.aws_vpc.default.cidr_block, 8, each.value) //went from 8 to 12
+  cidr_block = cidrsubnet(data.aws_vpc.default.cidr_block, 8, each.value) 
 
   tags = {
     Terraform = true
@@ -11,8 +11,8 @@ resource "aws_subnet" "private_subnets" {
 }
 
 # Security Groups
-resource "aws_security_group" "t3_tier" { //change //changed
-  name        = "t3_tier" //name to be changed //changed
+resource "aws_security_group" "t3_tier" { 
+  name        = "t3_tier" 
   description = "Allow inbound traffic"
   vpc_id      = data.aws_vpc.default.id
   ingress {
@@ -20,7 +20,7 @@ resource "aws_security_group" "t3_tier" { //change //changed
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["172.31.250.0/24", "120.29.76.169/32"]  //change this depending on your subnets 
+    cidr_blocks = ["172.31.250.0/24", "120.29.76.169/32"]  
   }
   egress {
     description = "Global Outbound"
@@ -30,24 +30,24 @@ resource "aws_security_group" "t3_tier" { //change //changed
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name    = "t3_tier" //name to be changed //changed
-    Purpose = "For dev"            //name to be changed //not realy yata
+    Name    = "t3_tier" 
+    Purpose = "For dev"            
   }
 }
 
 
-resource "aws_instance" "key_ko" {//change
-  ami                         = "ami-0ffac3e16de16665e" //change this for your  specific ami //already changed
+resource "aws_instance" "key_ko" {
+  ami                         = "ami-0ffac3e16de16665e" 
   instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.private_subnets["t3_tier"].id //change
-  security_groups             = [aws_security_group.t3_tier.id] //change
+  subnet_id                   = aws_subnet.private_subnets["t3_tier"].id 
+  security_groups             = [aws_security_group.t3_tier.id]
   associate_public_ip_address = true
-  //key_name                    = data.aws_key_pair.key_card //change
-  key_name                    = data.aws_key_pair.key_ko.key_name //change
+  //key_name                    = data.aws_key_pair.key_card 
+  key_name                    = data.aws_key_pair.key_ko.key_name
   #   iam_instance_profile        = "CloudWatchAgentServerPolicy"
 
   tags = {
-    Name = "t3_tier" //change
+    Name = "t3_tier" 
   }
 }
 
